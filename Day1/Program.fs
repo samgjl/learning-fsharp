3f532c95b640b02721ca8fad44e8d1894722e344
Sam Johnson-Lacoss <99389146+samgjl@users.noreply.github.com>
2024-10-11 01:00:02 -0700

    Learning F# - Part 1

---
+++ Day1/Program.fs
@@ -0,0 +1,23 @@
+﻿let fibIterative iters = 
+    let mutable val1 = 0
+    let mutable val2 = 1
+    for i in 1..iters do
+        let temp = val1 + val2
+        val1 <- val2
+        val2 <- temp
+    val2
+
+let rec fibRecurse x = 
+    if x <= 1 then 
+        1 
+    else 
+        (fibRecurse (x-1)) + (fibRecurse (x-2))
+
+
+[<EntryPoint>]
+let main argv =
+    // let iter = argv[0]
+    let iter = 4
+    printfn $"Fibonacci (iterative): {iter} iterations returns {fibIterative iter}"
+    printfn $"Fibonacci (recursive): {iter} iterations returns {fibRecurse iter}"
+    0
---
+++ Day2/Program.fs
@@ -0,0 +1,28 @@
+﻿/// Day 2: All Your Base
+/// https://exercism.org/tracks/fsharp/exercises/all-your-base
+
+let rec fromBase b num =
+    if (num%10) > b then -1 // Bad input
+    elif num = 0 then 0 // BC
+    else (num % 10) + (b * (fromBase b (num / 10))) // IC
+let rec toBase b num =
+    if b = 0 then -1 // Bad input
+    elif num = 0 then 0 // BC
+    else (num%b) + (10 * (toBase b (num/b))) // IC
+let convert oldBase newBase num = 
+    toBase newBase (fromBase oldBase num)
+
+let rec convertAll (fromList: list<int>) (toList: list<int>) (numList: list<int>): list<int> = 
+    if not (fromList.Length = toList.Length && toList.Length = numList.Length) then [-1] // Bad input
+    elif numList.IsEmpty || fromList.IsEmpty || toList.IsEmpty then [] // BC
+    else [convert fromList.Head toList.Head numList.Head] // IC
+        @ (convertAll fromList.Tail toList.Tail numList.Tail)
+
+[<EntryPoint>]
+let main argv =
+    let fromList = [2; 3; 10]
+    let toList = [10; 10; 3]
+    let numList = [00101; 00101; 10] // outputs 5; 10; 101 
+    let result = convertAll fromList toList numList
+    printfn $"{result}"
+    0
